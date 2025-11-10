c82b0133e86456ea15e47bf76d7bdaa9f4c37804
johcue <lianagar914@gmail.com>
2025-11-10 18:38:58 +0100

    US8 test case 1 red phase

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -122,8 +122,8 @@ class TestBowlingGame(unittest.TestCase):
 
     def test_spare_last_frame(self):
         game = BowlingGame()
-        game.add_frame(Frame(8, 2))
-        game.add_frame(Frame(5, 5))
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
         game.add_frame(Frame(7, 2))
         game.add_frame(Frame(3, 6))
         game.add_frame(Frame(4, 4))
@@ -131,7 +131,7 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(3, 3))
         game.add_frame(Frame(4, 5))
         game.add_frame(Frame(8, 1))
-        f = Frame(2, 6)
+        f = Frame(2, 8)
         game.add_frame(f)
         self.assertEqual(90, game.calculate_score())
 
