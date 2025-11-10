f3641b189b88c546414772dadc4613836697f04f
johcue <lianagar914@gmail.com>
2025-11-10 16:11:49 +0100

    US test case 1 RED phase

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,7 +1,12 @@
 import unittest
-
+from src.frame import Frame
+from src.bowling import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
+    def test_create_game(self):
+        f = Frame(1,5)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0))
         pass
